c6154d1bd6eedcb9a015f0ea31b5b6f637f9dcfb
hanmingcheng <12345678>
2019-03-01 13:31:51 +0800

    Initial commit

---
+++ src/controller/IndexController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/SpringMVC")
+public class IndexController {
+    @RequestMapping("/index")
+    public String index(ModelMap mv) {
+        mv.addAttribute("msg", "SpringMVC");
+        return "index";
+    }
+}
+
